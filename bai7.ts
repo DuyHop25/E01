c4813d07939d053ae5da13b96c82c6597f9629d9
DuyHop25 <127158780+DuyHop25@users.noreply.github.com>
2023-03-12 10:27:57 +0700

    Delete bai7.ts

--- bai7.ts
+++
@@ -1,9 +0,0 @@
-// Khai báo thư viện
-import { question } from "readline-sync";
-// Input
-let x:number = Number(question("Nhap x:"));
-let y:number = Number(question("Nhap y:"));
-// Xử lí tính toán 
-const S = Math.sqrt(Math.pow(x,y));
-// Output
-console.log(S);
