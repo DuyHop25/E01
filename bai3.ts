146368b995967b5e8297ca09c72d25180dd99a36
DuyHop25 <127158780+DuyHop25@users.noreply.github.com>
2023-03-12 10:27:07 +0700

    Delete bai3.ts

--- bai3.ts
+++
@@ -1,9 +0,0 @@
-// Khai bao thu vien
-import {question} from 'readline-sync';
-// Input
-let ten: string;
-ten = question("Nhap ten: ");
-// tinh toan 
-const LoiChao:String = "Hi, " + ten + "!";
-// Ouput
-console.log(LoiChao);
