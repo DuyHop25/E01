e83deaa15c417a13a56a2f054b98114eda5c8e61
DuyHop25 <127158780+DuyHop25@users.noreply.github.com>
2023-03-15 10:51:36 +0700

    Add files via upload
    Sửa bài 5

--- bai5.ts
+++ bai5.ts
@@ -3,13 +3,12 @@ import { question } from "readline-sync";
 // Input
 let N:number = Number(question("Nhap so xe: "));;
 // Xử lí tính toán 
-const dv:number = Math.floor(N%10);
-const chuc:number = Math.floor(N/10)%10;
-const tram:number = Math.floor(N/100)%10;
-const nghin:number = Math.floor(N/1000)%10;
-const chuc_nghin:number = Math.floor(N/10000);
-let nut:number = (dv+chuc+tram+nghin+chuc_nghin)
-if(nut >= 10)
-    nut = nut - 10
+const unit:number = Math.floor(N%10);
+const dozen:number = Math.floor(N/10)%10;
+const hundred:number = Math.floor(N/100)%10;
+const thousand:number = Math.floor(N/1000)%10;
+const tensOfThousands:number = Math.floor(N/10000);
+let sum:number = (unit+dozen+hundred+thousand+tensOfThousands)
+const nut :number = Math.floor(sum%10);
 // Output
-console.log("So xe:",N,"duoc",nut,"nut");
+console.log(nut,":nut");
